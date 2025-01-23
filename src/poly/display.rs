3f8be917131b7551cefc8be3246bfc25ae05baaf
Rasmus Buurman <rasmus@rbuurman.de>
2025-01-23 22:37:48 +0100

    Intoduce fold_displayring_poly and add some comments

--- src/poly/display.rs
+++ src/poly/display.rs
@@ -13,24 +13,29 @@ impl DisplayRing for R {}
 impl DisplayRing for Z {}
 impl<T: ZMod> DisplayRing for T {}
 
+/// A part of a displayable [Poly], useful for outputting
+///
+/// - `coefficient` can be `None` (if it is a One) or `Some` otherwise
+/// - `variable` can be `None` (degree 0), `Some(None)` (degree 1) or `Some(pot)` (degree `pot`)
+///
+/// See [Self::get_parts] for how to generate these parts from a [Poly].
 pub struct DisplayPart<R: DisplayRing> {
     pub coefficient: Option<R::Element>,
     pub variable: Option<Option<usize>>,
 }
 
 impl<R> DisplayPart<R>
-where 
+where
     R: DisplayRing,
     R::Element: Clone + PartialEq,
 {
+    /// Generate the [DisplayPart]s from a [Poly]
     pub fn get_parts(poly: &Poly<R>) -> Vec<DisplayPart<R>> {
         if poly.is_zero() {
-            return vec![
-                DisplayPart {
-                    coefficient: Some(poly.ring.zero()),
-                    variable: None,
-                }
-            ];
+            return vec![DisplayPart {
+                coefficient: Some(poly.ring.zero()),
+                variable: None,
+            }];
         }
 
         let mut parts = Vec::new();
@@ -47,11 +52,65 @@ where
                 _ => Some(Some(i)),
             };
 
-            parts.push(DisplayPart { coefficient, variable });
+            parts.push(DisplayPart {
+                coefficient,
+                variable,
+            });
         }
 
         parts
     }
+}
+
+/// Fold a displayable [Poly] into another type.
+///
+/// This uses [DisplayPart]s for "rendering".
+///
+/// - `init`: the seed value of the folding
+/// - `coeff`: if a coefficient can be displayed, do something to the accumulator
+/// - `var`: if a variable can be displayed, do something to the accumulator
+/// - `exp`: if a exponent can be displayed, do something to the accumulator
+/// - `sep`: do something to the accumulator to add a seperator
+pub fn fold_displayring_poly<R, O, C, V, E, S>(
+    poly: &Poly<R>,
+    init: O,
+    coeff: C,
+    var: V,
+    exp: E,
+    sep: S,
+) -> O
+where
+    R: DisplayRing,
+    R::Element: Clone + PartialEq,
+    C: Fn(&mut O, R::Element),
+    V: Fn(&mut O),
+    E: Fn(&mut O, usize),
+    S: Fn(&mut O),
+{
+    let parts = DisplayPart::get_parts(poly);
+    let len = parts.len();
+
+    parts
+        .into_iter()
+        .enumerate()
+        .fold(init, |mut acc, (i, part)| {
+            if let Some(c) = part.coefficient {
+                coeff(&mut acc, c);
+            }
+
+            if let Some(variable) = part.variable {
+                var(&mut acc);
+                if let Some(exponent) = variable {
+                    exp(&mut acc, exponent);
+                }
+            }
+
+            if i < len - 1 {
+                sep(&mut acc);
+            }
+
+            acc
+        })
 }
 
 impl<R> fmt::Display for Poly<R>
@@ -60,28 +119,23 @@ where
     R::Element: Clone + PartialEq + fmt::Display,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let parts = DisplayPart::get_parts(self);
-        let len = parts.len();
-
-        let string = parts.into_iter().enumerate().fold(String::new(), |mut acc, (i, part)| {
-            if let Some(e) = part.coefficient {
-                acc.push_str(&e.to_string());
-            }
-
-            if let Some(variable) = part.variable {
-                acc.push('x');
-                if let Some(exponent) = variable {
-                    acc.push('^');
-                    acc.push_str(&exponent.to_string());
-                }
-            }
-
-            if i < len - 1 {
-                acc.push_str(" + ");
-            }
-
-            acc
-        });
+        let string = fold_displayring_poly(
+            self,
+            String::new(),
+            |s, coeff| {
+                s.push_str(&coeff.to_string());
+            },
+            |s| {
+                s.push('x');
+            },
+            |s, exp| {
+                s.push('^');
+                s.push_str(&exp.to_string());
+            },
+            |s| {
+                s.push_str(" + ");
+            },
+        );
 
         write!(f, "{string}")
     }
